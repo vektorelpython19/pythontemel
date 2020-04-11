394ef389942da989398af7dffbc84a11fefcb9fe
Yekta Sökelen <yektasokelen@gmail.com>
2020-04-11 11:53:22 +0300

    SQL CLASS STRUCTURES ADDED!

--- Proje/Yekta/Calisma.py
+++ Proje/Yekta/Calisma.py
@@ -14,8 +14,35 @@ class TelefonDefter:
         self.db = sql.connect("/Users/yekta/PycharmProjects/pythontemel/Proje/IK.sqlite")
         self.cur = self.db.cursor()
 
+    def Ekleme(self):
+        try :
+            adi = input("Adi Giriniz : ")
+            soyadi = input("Soyadi Giriniz : ")
+            tel_no = input("Telefon Numarasin Giriniz : ")
+            sorgu = f"""INSERT INTO telefonlar (adi,soyadi,tel_no) VALUES ('{adi}','{soyadi}','{tel_no}')"""
+            self.cur.execute(sorgu)
+            self.db.commit()
+            return True
+        except Exception as Hata:
+            print("Hata Mesaji : ",Hata)
+            return False
+
+    def Listele(self):
+        try:
+            sorgu = """SELECT tel_id,adi,soyadi,tel_no FROM telefonlar"""
+            self.cur.execute(sorgu)
+            for tel_id,adi,soyadi,tel_no in self.cur.fetchall():
+                print(f"{tel_id}-{adi}-{soyadi}-{tel_no}")
+        except Exception as Hata :
+            print("Hata Mesaji : ",Hata)
 
     def __del__(self):
         self.cur.close()
         self.db.commit()
         self.db.close()
+
+
+telefonDefter = TelefonDefter()
+telefonDefter.Listele()
+telefonDefter.Ekleme()
+telefonDefter.Listele()
