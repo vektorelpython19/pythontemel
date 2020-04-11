ec9b6157becab1f166d47b4ae0f919d2ad3230ee
idilpython19 <61932702+idilpython19@users.noreply.github.com>
2020-04-11 11:11:18 +0300

    idil

--- Proje/Idil/Calisma.py
+++ Proje/Idil/Calisma.py
@@ -1,3 +1,9 @@
 import sqlite3 as sql
 #select * from personeller LIMIT 5
 db = sql.connect(r"Proje\IK.sqlite")
+cur = db.cursor()
+sorgu = """select adi,soyadi,email from personeller LIMIT 5"""
+cur.execute(sorgu)
+liste = cur.fetchall()
+for k1,k2,k3 in liste:
+    print(k1,k2,k3)
