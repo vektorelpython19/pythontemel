158947e6670373caf3ef95de42b5d637b746f10f
mertakkor01 <57759212+mertakkor01@users.noreply.github.com>
2020-04-11 11:21:32 +0300

    PERSONELID

--- Proje/Mert/Calisma.py
+++ Proje/Mert/Calisma.py
@@ -1,2 +1,10 @@
 import sqlite3 as sql
-db = sql.connect("DB\IK.sqlite")
+# select * from personeller LIMIT 5
+db = sql.connect("Proje\IK.sqlite")
+cur = db.cursor()
+perID = int(input("Personel ID giriniz: "))
+sorgu = f"""select adi,soyadi,email from personeller Where personel_id = {perID}"""
+cur.execute(sorgu)
+liste = cur.fetchall()
+for k1,k2,k3 in liste:
+    print(k1,k2,k3)
