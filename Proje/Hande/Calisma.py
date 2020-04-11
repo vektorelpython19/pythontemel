23f6867f841b5340c9c34843dde38b282686de1d
HandePython19 <61932656+HandePython19@users.noreply.github.com>
2020-04-11 11:57:55 +0300

    OOPÇevrim1

--- Proje/Hande/Calisma.py
+++ Proje/Hande/Calisma.py
@@ -1,7 +1,8 @@
 import sqlite3 as sql
 db=sql.connet("Proje\IK.sqlite")
 cur=db.cursor()
-sorgu="select adi,soyadi,email from personeller LIMIT 5"
+perId=int(input('Personel ID giriniz:'))
+sorgu=f"select adi,soyadi,email from Where personel_id={perId}"
 cur.execute(sorgu)
 liste=cur.fetchall()
 for k1,k2,k3 in liste:
