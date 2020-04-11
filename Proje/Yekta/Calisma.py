1426ef4b04d5f443824cc8124e6b9f1f3dcd4148
Yekta Sökelen <yektasokelen@gmail.com>
2020-04-11 11:20:50 +0300

    personel get personelId

--- Proje/Yekta/Calisma.py
+++ Proje/Yekta/Calisma.py
@@ -2,7 +2,8 @@ import sqlite3 as sql
 
 db = sql.connect("/Users/yekta/PycharmProjects/pythontemel/Proje/IK.sqlite")
 cur = db.cursor()
-sorgu = """ select adi,soyadi,email from personeller LIMIT 5 """
+perId = int(input("Personel Id Giriniz : "))
+sorgu = f""" select adi,soyadi,email from personeller where personel_id = {perId}"""
 cur.execute(sorgu)
 liste = cur.fetchall()
 for k1,k2,k3 in liste:
