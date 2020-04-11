2005b491dd5bb9c9f132dc0d205df2dfcfc9bb03
HandePython19 <61932656+HandePython19@users.noreply.github.com>
2020-04-11 11:08:20 +0300

    Update Calisma.py

--- Proje/Hande/Calisma.py
+++ Proje/Hande/Calisma.py
@@ -1,2 +1,8 @@
 import sqlite3 as sql
 db=sql.connet("Proje\IK.sqlite")
+cur=db.cursor()
+sorgu="select adi,soyadi,email from personeller LIMIT 5"
+cur.execute(sorgu)
+liste=cur.fetchall()
+for k1,k2,k3 in liste:
+  print(k1,k2,k3)
