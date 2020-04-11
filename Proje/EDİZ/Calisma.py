0f470aeea3e5ea183d382b4b1d507db319d13d43
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-11 11:20:12 +0300

    SELECTIDEDİZ

--- Proje/EDİZ/Calisma.py
+++ Proje/EDİZ/Calisma.py
@@ -1,3 +1,11 @@
 import sqlite3 as sql
 # select * from personeller LIMIT 5
 db = sql.connect("Proje\IK.sqlite")
+cur = db.cursor()
+perId = int(input("Personel ID giriniz:"))
+sorgu = f""" select adi,soyadi,email from personeller Where personel_id = {perId} """
+cur.execute(sorgu)
+liste = cur.fetchall()
+for k1,k2,k3 in liste:
+    print(k1,k2,k3)
+    
