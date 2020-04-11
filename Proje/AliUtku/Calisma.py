2cbc96598f28b50bfedac187f6d60fe6d4e55cf8
utkuakcan <61932699+utkuakcan@users.noreply.github.com>
2020-04-11 11:58:11 +0300

    Update Calisma.py

--- Proje/AliUtku/Calisma.py
+++ Proje/AliUtku/Calisma.py
@@ -1,36 +1,47 @@
 import sqlite3 as sql
-#db = sql.connect(r"Proje\IK.sqlite")
+
+#db = sql.connect("/Users/yekta/PycharmProjects/pythontemel/Proje/IK.sqlite")
 #cur = db.cursor()
-#perId = int(input("Personel ID giriniz:"))
-#sorgu = f""" select adi,soyadi,email from personeller Where personel_id = {perId} """
+#perId = int(input("Personel Id Giriniz : "))
+#sorgu = f""" select adi,soyadi,email from personeller where personel_id = {perId}"""
 #cur.execute(sorgu)
 #liste = cur.fetchall()
 #for k1,k2,k3 in liste:
 #    print(k1,k2,k3)
 
-
-
 class TelefonDefter:
-    def__init__(self):
-       self.db = sql.connect("Proje\IK.sqlite")
-       self.cur = self.db.cursor()
-    
+    def __init__(self):
+        self.db = sql.connect("/Users/yekta/PycharmProjects/pythontemel/Proje/IK.sqlite")
+        self.cur = self.db.cursor()
+
     def Ekleme(self):
-        adi= input("Adı Giriniz:")
-        soyadi = input("Soyadi Giriniz:")
-        tel_no = input("Telefon Numarası Giriniz:")
-        sorgu = f"""
-        INSERT INTO telefonlar (adi,soyadi,tel_no) VALUES ('{}','{}','{}')"""
-        self.cur.execute(sorgu)
+        try :
+            adi = input("Adi Giriniz : ")
+            soyadi = input("Soyadi Giriniz : ")
+            tel_no = input("Telefon Numarasin Giriniz : ")
+            sorgu = f"""INSERT INTO telefonlar (adi,soyadi,tel_no) VALUES ('{adi}','{soyadi}','{tel_no}')"""
+            self.cur.execute(sorgu)
+            self.db.commit()
+            return True
+        except Exception as Hata:
+            print("Hata Mesaji : ",Hata)
+            return False
+
+    def Listele(self):
+        try:
+            sorgu = """SELECT tel_id,adi,soyadi,tel_no FROM telefonlar"""
+            self.cur.execute(sorgu)
+            for tel_id,adi,soyadi,tel_no in self.cur.fetchall():
+                print(f"{tel_id}-{adi}-{soyadi}-{tel_no}")
+        except Exception as Hata :
+            print("Hata Mesaji : ",Hata)
+
+    def __del__(self):
+        self.cur.close()
         self.db.commit()
-        return True
-    except Exception as hata:
-        print("Hata Mesajı:",hata)
-        return False
+        self.db.close()
 
-
-
-       def__del__(self):
-          self.cur.close()
-          self.db.commit()
-          self.db.close() 
+telefonDefter = TelefonDefter()
+telefonDefter.Listele()
+telefonDefter.Ekleme()
+telefonDefter.Listele()
