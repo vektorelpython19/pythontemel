078728e2fb2ede0b10f1027c2a2a68cd31759f94
utkuakcan <61932699+utkuakcan@users.noreply.github.com>
2020-04-12 12:38:28 +0300

    Update OrnekGUI.py

--- Proje/AliUtku/OrnekGUI.py
+++ Proje/AliUtku/OrnekGUI.py
@@ -16,16 +16,23 @@ txtPass.grid(column=1,row=1)
 def FonkGiris():
     if txtUser.get() and txtPass.get():
         if txtUser.get() == "Utku" and txtPass.get() == "abcde":
-            print("Giriş Başarılı")
+            sonuc["text"] = "giriş Başarılı"
         else:
-            print("Giriş Başarısız")
+            sonuc["text"] = "Giriş Başarısız"
 
 def Temizle():
-    pass
+    txtUser.delete(0,END)
+    txtUser.insert(0,"")
+    txtPass.delete(0,ENd)
+    txtPass.insert(0,"")
+
 
 dugme1 = Button(pencere,text="Giriş Yap", width=30,command=FonkGiris)
 dugme1.grid(column=0,row=2)
 dugme2 = Button(pencere,text="Temizle",width=30,command=Temizle)
 dugme2.grid(column=1,row=2)
+
+sonuc = Label(pencere,text="",width=40)
+sonuc.grid(column=0,row=3)
 
 pencere.mainloop()
