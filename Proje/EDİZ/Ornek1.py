1c8dd3352f3cd9e1a6a43374977939c1cce014b7
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-12 11:42:39 +0300

    GUIEklendiEdiz

--- Proje/EDİZ/Ornek1.py
+++ Proje/EDİZ/Ornek1.py
@@ -1,3 +1,7 @@
-import tkinter as tk
-pencere = tk.Tk()
-pencere.mainloop()
+import random as rnd
+metin = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum vestibulum volutpat. Nullam cursus placerat purus, quis ultrices lacus pretium at. Nullam interdum auctor arcu, in blandit turpis tristique ac. Vestibulum tristique lacinia cursus. Vivamus aliquet feugiat ante. Suspendisse elementum hendrerit arcu sit amet pulvinar. Cras tempus volutpat dignissim. Donec non libero vitae felis porta ultricies. Cras tincidunt ante et elit egestas viverra. Maecenas nunc ante, ornare vitae ligula ac, malesuada vestibulum massa. Curabitur viverra, risus eu hendrerit vehicula, eros elit maximus velit, et dictum felis felis quis ex. Duis justo nisl, semper eu feugiat vel, suscipit eget purus. Nunc elementum convallis pretium. Suspendisse tincidunt lacus augue, ut vehicula libero blandit eu.
+Sed purus lorem, congue eu tempor in, suscipit sit amet lectus. In semper egestas aliquam. Morbi iaculis lorem in lorem tincidunt, in pretium mi tempor. Etiam non mi convallis, maximus nunc sollicitudin, vestibulum elit. Pellentesque semper mi placerat arcu hendrerit molestie. Aenean et nunc nibh. Duis quis libero id ligula tincidunt eleifend id id nibh. Mauris euismod vehicula nibh et vestibulum.
+"""
+for i in range(50):
+    print(metin[rnd.randint(0,len(metin)):rnd.randint(0,len(metin))],rnd.randint(0,5),sep=";",end="\n",file=open("defter.csv","r+"))
