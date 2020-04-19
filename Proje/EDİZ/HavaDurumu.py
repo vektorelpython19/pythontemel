09393392d0840ae33cd8c1cf228e3bec64c54ba9
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-19 13:20:59 +0300

    19042020

--- Proje/EDİZ/HavaDurumu.py
+++ Proje/EDİZ/HavaDurumu.py
@@ -7,11 +7,11 @@ soup = BeautifulSoup(sayfa.content,'html.parser')
 
 tarih = soup.findAll("th",class_="th")
 
-# for i in range(1,len(tarih)):
-#     print(tarih[i].get_text())
-#     merkez = soup.findAll("th",class_="alt_th")
-#     for j in range(1,len(merkez)):
-#         print(merkez[j].get_text())
+for i in range(1,len(tarih)):
+    print(tarih[i].get_text())
+    merkez = soup.findAll("th",class_="alt_th")
+    for j in range(1,len(merkez)):
+        print(merkez[j].get_text())
 
 print(tarih[1].get_text())
 merkez = soup.findAll("th",class_="alt_th")
