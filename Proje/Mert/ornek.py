9b67ff9596b46f293b724596aa778f24ada1d030
mertakkor01 <57759212+mertakkor01@users.noreply.github.com>
2020-04-19 10:58:49 +0300

    19042020

---
+++ Proje/Mert/ornek.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+sayfa = requests.get("https://raw.githubusercontent.com/vektorelpython19/pythontemel/master/Proje/Hande/Ornek.html")
+soup = BeautifulSoup(sayfa.content,'html.parser')
+print(soup.find_all('p')[0].get_text())
+# print(sayfa.content) # ED%C4%B0Z
+# print(soup.prettify())
+# print(list(soup.children))
+
+# liste = [type(item) for item in list(soup.children)]
+
+# html = list(soup.children)[0]
+# body = list(html.children)[3]
+# p=list(body.children)[1]
+
+# print(p.get_text())
