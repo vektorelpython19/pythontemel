acffcf11e2f283c39e12f4d637a6cd1e23e82393
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-19 11:04:34 +0300

    19042020

--- Proje/EDİZ/ornek.py
+++ Proje/EDİZ/ornek.py
@@ -2,9 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 sayfa = requests.get("https://raw.githubusercontent.com/vektorelpython19/pythontemel/master/Proje/Hande/Ornek.html")
 soup = BeautifulSoup(sayfa.content,'html.parser')
-# print(sayfa.content)
+print(soup.find_all('p')[0].get_text())
+
+# print(sayfa.content) # ED%C4%B0Z
 # print(soup.prettify())
 # print(list(soup.children))
+# bs4.element.tag
+# liste = [type(item) for item in list(soup.children)]
 
-liste = [type(item) for item in list(soup.children)]
-print(liste)
+# html = list(soup.children)[0]
+# print(html)
+# body = list(html.children)[3]
+# print(body)
+# p=list(body.children)[1]
+# print(p)
+# print(p.get_text())
+
