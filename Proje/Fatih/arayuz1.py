69f429b6dac3d0703fc798ff1a516394a9cd7b3e
e1077775 <61932662+e1077775@users.noreply.github.com>
2020-04-19 09:37:34 +0300

    Create arayuz1.py

---
+++ Proje/Fatih/arayuz1.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5 import uic
+
+class Uygulama(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi (r"Proje\Fatih\arayuz1.ui",self)
+        self.Goster()
+
+    def Goster(self):
+        self.show()
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    ex = Uygulama()
+    sys.exit(app.exec())
