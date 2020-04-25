df6cb0e2b8f9703c699fd12043a66086216e0351
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-04-25 11:07:52 +0300

    25042020

--- Proje/EDİZ/Emlak/AnaMenu.py
+++ Proje/EDİZ/Emlak/AnaMenu.py
@@ -3,15 +3,25 @@ import os
 sys.path.append(os.getcwd()+os.sep+"DB")
 from PyQt5.QtWidgets import QApplication,QMainWindow,QMessageBox
 from PyQt5 import uic
-from DB1 import DataEmlak
+from DB.DB1 import DataEmlak
 
 class Uygulama(QMainWindow):
     def __init__(self):
         super().__init__()
-        #1.çağırma
         uic.loadUi(r"Proje\EDİZ\Emlak\ProjeUI_1.ui",self)
+        self.dbEmlak = DataEmlak()
+        self.Goster()
 
+    def Goster(self):
+        self.ilDoldur()
+        self.prgCekim.setValue(0)
+        self.show()
 
+    def ilDoldur(self):
+        liste = self.dbEmlak.ilListele()
+        self.cmbil.addItem("-1","Seçiniz")
+        for item in liste:
+            self.cmbil.addItem(str(item[0]),item[1])
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
