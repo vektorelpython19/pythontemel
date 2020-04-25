4441605f24fa08a8b2adee4e5cc5fb810268706b
Yekta Sökelen <yektasokelen@gmail.com>
2020-04-25 10:54:13 +0300

    SQL Added!

--- Proje/Yekta/Emlak/AnaMenu.py
+++ Proje/Yekta/Emlak/AnaMenu.py
@@ -1,13 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+import os
+sys.path.append("/Users/yekta/PycharmProjects/pythontemel/DB")
+from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5 import uic
+from DB1 import DataEmlak
 
 class Uygulama(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi(r"/Users/yekta/PycharmProjects/pythontemel/Proje/Yekta/Emlak/ProjeUI_1.ui",self)
-
-
 
 
 if __name__ == "__main__":
