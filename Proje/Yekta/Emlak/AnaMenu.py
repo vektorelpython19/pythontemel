3a7befd9be24c7673959d64e89ca9b3896134c61
Yekta Sökelen <yektasokelen@gmail.com>
2020-04-25 11:06:38 +0300

    SQL Added!

--- Proje/Yekta/Emlak/AnaMenu.py
+++ Proje/Yekta/Emlak/AnaMenu.py
@@ -9,7 +9,19 @@ class Uygulama(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi(r"/Users/yekta/PycharmProjects/pythontemel/Proje/Yekta/Emlak/ProjeUI_1.ui",self)
+        self.dbEmlak = DataEmlak
+        self.Goster()
 
+    def Goster(self):
+        self.ilDoldur()
+        #self.prgCekim.setValue(0)
+        self.show()
+
+    def ilDoldur(self):
+        liste =  self.dbEmlak.ilListele()
+        self.cmbil.addItem("-1","Seciniz")
+        for item in liste:
+            self.cmbil.addItem(str(item[0],item[1]))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
