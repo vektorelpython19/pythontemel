a2d62e4902404d9243535e838760b65a50df8653
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-05-03 10:40:38 +0300

    03052020

--- OpenCV/Ediz/OpenCv1.py
+++ OpenCV/Ediz/OpenCv1.py
@@ -5,7 +5,10 @@ from matplotlib import pyplot as plt
 # BGR
 
 img = cv2.imread(r"OpenCV\resim.jpg")
-print(img.shape)
-cv2.imshow("resmi ekledim",img)
+gri = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+print(gri[55,55])
+print(gri.shape)
+
+cv2.imshow("resmi ekledim",gri)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
