1ab53fe78a644516aea3668afec874364870df98
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2020-05-03 10:53:32 +0300

    03052020

--- OpenCV/Ediz/OpenCv2.py
+++ OpenCV/Ediz/OpenCv2.py
@@ -4,11 +4,12 @@ from matplotlib import pyplot as plt
 # RGB 
 # BGR
 
-img = cv2.imread(r"OpenCV\resim.jpg")
+img = cv2.imread(r"OpenCV\kitap.jpg")
 gri = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-print(gri[55,55])
-print(gri.shape)
-
+# retval,treshold = cv2.threshold(gri,12,255,cv2.THRESH_BINARY)
+th = cv2.adaptiveThreshold(gri,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,155,1)
+cv2.imshow("eşiklenmiş",th)
 cv2.imshow("resmi ekledim",gri)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
